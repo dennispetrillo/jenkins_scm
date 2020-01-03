4cc6c07df204894695d1ddb26d627930868b9778
dennispetrillo <7047278+dennispetrillo@users.noreply.github.com>
2020-01-03 16:04:55 -0500

    update

--- testScript.sh
+++ testScript.sh
@@ -2,5 +2,5 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "this is the fourth line I added"
+echo "this is the fifth line I added"
 echo "completed"
